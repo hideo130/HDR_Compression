81634766892e255de46af0d7297e2f1208412ea7
Adachi Hideo <higan.ibn5100@gmail.com>
2020-08-21 23:53:32 +0900

    add test
    微分係数と定数項を目視で確認するテストを書いた．
    確認も自動化したい．

---
+++ test/tests.py
@@ -0,0 +1,28 @@
+import numpy as np
+from logzero import logger
+from pathlib import Path
+import sys
+import_path = Path(__file__).resolve().parents[1]
+sys.path.append(str(import_path))
+from hdr_comp import get_coeff, get_b
+
+
+def print_coeff():
+    coeff_x, coeff_y = get_coeff(3, 3)
+    logger.info(f"{coeff_x=}")
+    logger.info(f"{coeff_y=}")
+    logger.info(f"{coeff_x.shape=}")
+    logger.info(f"{coeff_y.shape=}")
+
+
+def print_b():
+    img = np.arange(25).reshape((5, 5))
+    img[1:-1,1:-1] = 0
+    tmp = np.zeros((3,3))
+    get_b(img, tmp, tmp)
+    
+
+
+if __name__ == "__main__":
+    # print_coeff()
+    print_b()
